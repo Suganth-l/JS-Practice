45e7545e7a9433ee4c58e85fc2cc91ca3bf7b3e2
Suganth L <suganthlingappasamy@gmail.com>
2026-07-06 12:38:03 +0530

    Function Declarations

---
+++ JS Files/declarefunc.js
@@ -0,0 +1,74 @@
+//declaring function
+
+//function declaration
+function square() {
+  let num = 10;
+  console.log(num * num);
+}
+square();
+
+//function Expression
+let square2 = function (num) {
+  console.log(`The square of ${num} is ${num * num}`);
+};
+square2(5);
+
+//Arrow function
+let square3 = (num) => console.log(num * num);
+square3(3);
+
+//Anonymous Function
+(function () {
+  console.log(`Thank You`);
+})();
+
+function largest(a, b, c) {
+  if (a > b && a > c) {
+    console.log(`The largest number is ${a}`);
+  }
+  if (b > a && b > c) {
+    console.log(`The largest number is ${b}`);
+  } else {
+    console.log(`The largest number is ${c}`);
+  }
+}
+
+const largest2 = function (a, b, c) {
+  if (a > b && a > c) {
+    console.log(`The largest number is ${a}`);
+  }
+  if (b > a && b > c) {
+    console.log(`The largest number is ${b}`);
+  } else {
+    console.log(`The largest number is ${c}`);
+  }
+};
+
+const largest3 = (a, b, c) => {
+  if (a > b && a > c) {
+    console.log(`The largest number is ${a}`);
+  }
+  if (b > a && b > c) {
+    console.log(`The largest number is ${b}`);
+  } else {
+    console.log(`The largest number is ${c}`);
+  }
+};
+
+largest(3, 5, 2);
+largest2(3, 9, 6);
+largest3(7, 9, 12);
+
+(function () {
+    let a = 2;
+    let b = 9;
+    let c = 71;
+   if (a > b && a > c) {
+    console.log(`The largest number is ${a}`);
+  }
+  if (b > a && b > c) {
+    console.log(`The largest number is ${b}`);
+  } else {
+    console.log(`The largest number is ${c}`);
+  }
+}) ();
