669f42f8dacfc52315dbfeef7289ac073f3f7aaa
Suganth L <suganthlingappasamy@gmail.com>
2026-07-07 18:26:03 +0530

    Arrays in JavaScript

---
+++ JS Files/array.js
@@ -0,0 +1,10 @@
+//Arrays in JS
+let arr = [101, "Abhi", "ECE", 250, true];
+console.log(arr[3]);
+
+let arr1 = new Array("Sugan", "Abhi", "Rohan");
+arr1.push("Siva");
+console.log(arr1);
+arr1.unshift("Ravi");
+console.log(arr1);
+console.log(arr1.pop());
