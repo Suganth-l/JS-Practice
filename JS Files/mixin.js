246a0e3e619449df23352759b1b8e17175a6e1a2
Suganth L <suganthlingappasamy@gmail.com>
2026-07-20 17:36:57 +0530

    Mixins in JS

---
+++ JS Files/mixin.js
@@ -0,0 +1,18 @@
+const flyMixin = {
+    fly() {
+        console.log("Flying..");
+    }
+};
+
+const swimMixin = {
+    swim() {
+        console.log("Swimming..");
+    }
+};
+
+class Duck { }
+//Mixins
+Object.assign(Duck.prototype, flyMixin, swimMixin);
+const duck = new Duck();
+duck.fly();
+duck.swim();
